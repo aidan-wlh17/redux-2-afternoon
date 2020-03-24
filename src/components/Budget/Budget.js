288dcb9430092308329c14d26225954f48aff65a
Aidan Trujillo <aidantrujillo@Aidans-MacBook-Pro.local>
2020-03-23 18:28:35 -0600

    first commit done

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -6,25 +6,34 @@ import AddPurchase from './../shared/AddPurchase';
 import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
+import {connect} from 'react-redux'
+import {requestUserData} from './../../ducks/userReducer'
+import {requestBudgetData, addPurchase, removePurchase} from './../../ducks/budgetReducer'
 import './Budget.css';
 
 
 class Budget extends Component {
 
+  componentDidMount() {
+    this.props.requestUserData()
+    this.props.requestBudgetData()
+  }
+
   render() {
+    const {loading} = this.props.budget
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={this.props.user.firstName} lastName={this.props.user.lastName}/>
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              <AddPurchase addPurchase={this.props.addPurchase}/>
+              <DisplayPurchases purchases={this.props.budget.purchases} removePurchase={this.props.removePurchase}/>
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={this.props.budget.purchases} budgetLimit={this.props.budgetLimit}/>
+              <Chart2 purchases={this.props.budget.purchases}/>
             </div>
           </div>
         </div>
@@ -33,4 +42,11 @@ class Budget extends Component {
   }
 }
 
-export default Budget;
+const mapStateToProps = reduxState => {
+  return {
+    budget: reduxState.budget,
+    user: reduxState.user
+  }
+}
+
+export default connect(mapStateToProps, {requestUserData, requestBudgetData, addPurchase, removePurchase})(Budget)
